2844cfa5cbb7a49650ff39e54e3820a94c547873
batistadaniel <batistanegobiz@gmail.com>
2026-06-25 08:30:34 -0300

    troca BrowserRouter por HashRouter

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import MapPage from '../pages/MapPage'
 
 function AppRoutes() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<MapPage />} />
-        {/* Caso queira adicionar novas páginas no futuro: <Route path="/linhas" element={<Linhas />} /> */}
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
